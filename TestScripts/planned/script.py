cb4472a11b3c58bc0c91a9af3364e37aa9359c27
Arne <arnev.c.23@gmail.com>
2024-09-26 11:16:49 +0200

    try to find trips from start to destination

--- TestScripts/planned/script.py
+++ TestScripts/planned/script.py
@@ -5,6 +5,16 @@ import os
 import shutil
 from io import BytesIO
 
+start = input("input start station: ")
+if start == "" or start == None:
+    print("start station needs to be defined")
+    quit()
+
+stop = input("input stop station: ")
+if stop == "" or stop == None:
+    print("stop station needs to be defined")
+    quit()
+
 uri = ""
 
 try:
@@ -35,3 +45,50 @@ stops = pd.read_csv("gtfs_data/stops.txt")
 stop_times = pd.read_csv("gtfs_data/stop_times.txt")
 trips = pd.read_csv("gtfs_data/trips.txt")
 routes = pd.read_csv("gtfs_data/routes.txt")
+
+
+def get_stop_id_by_name(stop_name, stops_df):
+    stop_row = stops_df[
+        stops_df["stop_name"].str.contains(stop_name, case=False, na=False)
+    ]
+    if stop_row.empty:
+        return None
+    return stop_row["stop_id"].values[0]
+
+
+origin_stop_id = get_stop_id_by_name(start, stops)
+destination_stop_id = get_stop_id_by_name(stop, stops)
+
+if origin_stop_id is None or destination_stop_id is None:
+    print("Origin or destination station not found")
+    quit()
+
+origin_trips = stop_times[stop_times["stop_id"] == origin_stop_id]
+destination_trips = stop_times[stop_times["stop_id"] == destination_stop_id]
+
+possible_trips = pd.merge(
+    origin_trips, destination_trips, on="trip_id", suffixes=("_origin", "_destination")
+)
+
+possible_trips = possible_trips[
+    possible_trips["stop_sequence_origin"] < possible_trips["stop_sequence_destination"]
+]
+
+possible_trips_with_details = pd.merge(possible_trips, trips, on="trip_id")
+possible_trips_with_details = pd.merge(
+    possible_trips_with_details, routes, on="route_id"
+)
+
+print("Possible trips from origin to destination:")
+print(
+    possible_trips_with_details[
+        [
+            "route_id",
+            "route_short_name",
+            "route_long_name",
+            "trip_id",
+            "arrival_time_origin",
+            "arrival_time_destination",
+        ]
+    ]
+)
